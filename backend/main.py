d277ee5276e6b70ac9f1218bfdfafa15f03033d4
Edwin Castro <edwinsantiagocastrobuitrago@gmail.com>
2025-07-08 00:00:04 -0500

    uso de docker contenedor funcional local

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,7 @@ import pytesseract
 from PIL import Image
 import io
 import os
-
+import re
 app = FastAPI()
 
 app.add_middleware(
@@ -34,7 +34,12 @@ async def upload_ocr(file: UploadFile = File(...)):
         image = Image.open(image_stream)
 
         extracted_text = pytesseract.image_to_string(image, lang='spa')
-
+        extracted_text = extracted_text.replace('\f', '')
+        extracted_text = re.sub(r'\n+', '\n', extracted_text)
+        extracted_text = '\n'.join([line.strip() for line in extracted_text.split('\n')])
+        extracted_text = re.sub(r' {2,}', ' ', extracted_text)
+        extracted_text = extracted_text.strip()
+        
         if not extracted_text.strip():
             return JSONResponse(
                 content={"extracted_text": "No se pudo detectar texto en la imagen."}
